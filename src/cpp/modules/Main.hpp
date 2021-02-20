0de8995d99d429418077ac39f0a114289face5fc
chw1119 <chahyunwooa@naver.com>
2021-02-20 18:08:13 +0900

    fire 구조

---
+++ src/cpp/modules/Main.hpp
@@ -0,0 +1,9 @@
+#ifndef __MAIN
+#define __MAIN
+
+class Flappy::Main {
+	void API_ENTRY inline static main(int argc, char** argv);
+};
+	
+
+#endif
---
+++ src/cpp/modules/module.h
@@ -0,0 +1,69 @@
+#ifndef __moduels
+#define __modules
+
+#ifndef API_ENTRY
+
+
+#ifdef __DLL
+
+#define API_ENTRY __declspec(dllexport)
+
+#elif 
+
+#define API_ENTRY __declspec(dllimport)
+
+#endif
+
+
+#endif
+
+namespace Flappy {
+
+	class Main;
+
+	namespace io {
+
+		namespace packet {
+			
+			class Packet;
+
+			class LoginPacket;
+
+			class BirdInformationPacket;
+
+			class PacketTranslator;
+
+		}
+
+	}
+
+	namespace game {
+
+		namespace sprite {
+
+			class Sprite;
+
+			class Pipe;
+
+			class Bird;
+
+			class Ground;
+
+			class Cloud;
+		}
+
+		namespace ui {
+			class Button;
+
+			class Images;
+		}
+
+		namespace contents {
+
+		}
+	}
+}
+
+#include "./Main.hpp";
+
+#endif
